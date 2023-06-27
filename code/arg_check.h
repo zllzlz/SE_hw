7c9ad536d4bbdbdc5c85f636bd4fb4e5863c4d16
kjy <2979267378@qq.com>
2023-06-27 11:02:10 +0800

    数独求解

---
+++ code/arg_check.h
@@ -0,0 +1,67 @@
+#pragma once
+#include<iostream>
+#include<fstream>
+#include<string.h>
+#include<stdio.h>
+#include<tchar.h>
+using namespace std;
+
+
+/*
+* 类型1：sudoku.exe -c 20 [表示生成20个数独终盘]
+* 类型2：sudoku.exe -s game.txt [表示从game.txt读取若干个数独游戏,并给出其解答，生成到sudoku.txt中]
+* 类型3：sudoku.exe -n 1000 [表示生成1000个数独游戏]
+* 类型4：sudoku.exe -n 1000 -m 1 [表示生成1000个简单数游戏，只有m和n一起使用才认为参数无误，否则请报错,m表示生成游戏的难度]
+* 类型5：sudoku.exe -n 20 -r 20~55 [表示生成20个挖空数在20到55之间的数独游戏，只有r和n一起使用才认为参数无误，否则请报错]
+* 类型6：sudoku.exe -n 20 -u [表示生成20个解唯一的数独游戏,只有u和n一起使用才认为参数无误，否则请报错]
+* 类型7：错误输入
+*/
+
+
+
+
+//处理输入参数的类
+class ArgCheck {
+public:
+	//初始化函数
+	ArgCheck() {};
+	ArgCheck(int argc, char* argv[]);
+
+	//分析输入参数
+	void analy_Arg();
+
+	//参数输入是否合法
+	bool valid_arg();
+
+	//打印参数（调试使用）
+	void print_arg();
+
+	//返回参数类型
+	int get_ArgType();
+
+	//返回参数值
+	int get_c();
+	int get_n();
+	int get_m();
+	int get_r_left();
+	int get_r_right();
+
+private:
+	//参数数量
+	int argc;
+	//参数字符串
+	char** argv;
+
+	//参数类型
+	int arg_type;
+	//数独终局的个数 1-1000000
+	int c;
+	//数独游戏的个数 1-10000
+	int n;
+	//难度 1-3
+	int m;
+	//挖空的数量 20-55
+	int r_left;
+	int r_right;
+
+};
---
+++ code/shudu.cpp
@@ -0,0 +1,68 @@
+#pragma once
+#include<iostream>
+#include"arg_check.h"
+#include"shudu_func.h"
+#include"arg_check.cpp"
+#include"shudu_func.cpp"
+using namespace std;
+
+
+
+//生成不重复的数独终局至文件
+//生成指令：shudu.exe-c100 
+//其他参数：
+/*
+* sudoku.exe -c 20 [表示生成20个数独终盘]
+* sudoku.exe -s game.txt [表示从game.txt读取若干个数独游戏,并给出其解答，生成到sudoku.txt中]
+* sudoku.exe -n 1000 [表示生成1000个数独游戏]
+* sudoku.exe -n 1000 -m 1 [表示生成1000个简单数游戏，只有m和n一起使用才认为参数无误，否则请报错,m表示生成游戏的难度]
+* sudoku.exe -n 20 -r 20~55 [表示生成20个挖空数在20到55之间的数独游戏，只有r和n一起使用才认为参数无误，否则请报错]
+* sudoku.exe -n 20 -u [表示生成20个解唯一的数独游戏,只有u和n一起使用才认为参数无误，否则请报错]
+*/
+
+int main(int argc, char* argv[]) {
+	//参数识别
+	ArgCheck arg_check = ArgCheck(argc, argv);
+	arg_check.analy_Arg();
+	if (!arg_check.valid_arg()) {
+		printf("输入有误，请检查输入\n");
+		return 0;
+	}
+	arg_check.print_arg();
+
+	int type = arg_check.get_ArgType();
+	ShuDu shudu_op = ShuDu(arg_check);
+	//根据识别的参数进行相应的操作
+	switch (type) {
+		//sudoku.exe -c 20
+	case 1:
+		shudu_op.gen_shudu_ending(arg_check.get_c());
+		break;
+		//sudoku.exe -s game.txt
+	case 2:
+		//添加数独游戏的计算
+		shudu_op.solve_shudu();
+		break;
+		//sudoku.exe -n 1000 
+	case 3:
+		shudu_op.gen_shudu_game(arg_check.get_m());
+		break;
+		//sudoku.exe -n 1000 -m 1 
+	case 4:
+		shudu_op.gen_shudu_game(arg_check.get_m());
+		break;
+		//sudoku.exe -n 20 -r 20~55
+	case 5:
+		shudu_op.gen_shudu_game(arg_check.get_r_left());
+		break;
+		//sudoku.exe -n 20 -u
+	case 6:
+		shudu_op.gen_shudu_game(arg_check.get_r_left());
+		break;
+		//错误输入
+	case 7:
+		break;
+	}
+
+	return 0;
+}
